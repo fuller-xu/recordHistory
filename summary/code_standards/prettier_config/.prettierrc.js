2a7c49cf3daae2df70b0681874402f361916b191
xufeibaiseng <xufei@baiseng.com>
2019-09-30 22:35:15 +0800

    add webstorm & vscode common config

---
+++ summary/code_standards/prettier_config/.prettierrc.js
@@ -0,0 +1,17 @@
+// 放在项目根目录的 .prettierrc.js 文件
+module.exports = {
+  // 单行最大长度
+  printWidth: 100,
+  // 设置编辑器每一个水平缩进的空格数
+  tabWidth: 2,
+  // 在句尾添加分号
+  semi: false,
+  // 使用单引号
+  singleQuote: true,
+  // 在任何可能的多行中输入尾逗号。
+  trailingCommas: 'es5',
+  // 在对象字面量声明所使用的的花括号后（{）和前（}）输出空格
+  bracketSpacing: true,
+  // 为单行箭头函数的参数添加圆括号。
+  alwaysParens: 'always'
+}
